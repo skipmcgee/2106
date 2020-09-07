f81f6108e9e0b1e6644687ba041c498ba9418806
Skip McGee <70141823+skipmcgee@users.noreply.github.com>
2020-09-07 09:45:55 +0300

    Update taxapp.py

--- taxapp.py
+++ taxapp.py
@@ -7,8 +7,32 @@
 #
 ##################################################################################
 
-# Import statements for all standard modules
-pip install pdfforms
-import pdfforms
-pdfforms inspect f2106.pdf
-pdfforms fill userdata.csv
+# Import statements
+try:
+    import pip
+except ImportError:
+    try:
+        subprocess.call(["yum", "install", "-y", "rh-python36-python-pip"])
+        import pip
+    except:
+        try:
+            subprocess.call(["yum", "install", "-y", "python-pip"])
+            import pip
+        except:
+            pass
+# import pdfforms
+try:
+    import pdfforms
+except ImportError:
+    try:
+        subprocess.call(["yum", "install", "-y","pdfforms"])
+        import pdfforms
+    except:
+      try:
+        subprocess.call(["pip", "install", "pdfforms"])
+    except:
+        print("Error with installing pdfforms, unable to install... exiting...")
+        exit()
+
+# pdfforms inspect f2106.pdf
+# pdfforms fill userdata.csv
