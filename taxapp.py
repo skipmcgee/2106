e06425b4b0d19895f6a6a1db5e90a9c10f79758e
Skip McGee <70141823+skipmcgee@users.noreply.github.com>
2020-09-07 09:09:18 +0300

    Create taxapp.py

---
+++ taxapp.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+##################################################################################
+#
+# Created by Skip McGee for DJC2 20.2 AKA "The Looters"
+#
+#
+##################################################################################
+
+# Import statements for all standard modules
+import subprocess
